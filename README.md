8f18f3af1d6d9049fb4e4e516443a05d3275d1a9
Avesh <aavesh88@gmail.com>
2024-02-04 12:23:17 +0530

    adding flask server

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ToDo
 ToDo app using GRPC
+
+# How to Run
+```
+python server.py
+python -m flask --app client run
+```
+
+## Get All todos
+/
+
+Get all todos
+
+## Add todo
+/todo/<text>
+
+Replace <text> with content of Todo.
+
+UUID is returned on successfully creation of Todo
--- client.py
+++ client.py
@@ -1,30 +1,26 @@
+from flask import Flask, render_template
+
 import grpc
-import logging
 import todo_pb2_grpc
 import todo_pb2
 
 
-def add_todo(stub, text):
+app = Flask(__name__)
+channel = grpc.insecure_channel("localhost:40000")
+client = todo_pb2_grpc.TodoStub(channel)
+
+
+@app.route("/todo/<text>")
+def add_todo(text):
     todo_text = todo_pb2.TodoText(text=text)
-    todo_item = stub.createTodo(todo_text)
-    return todo_item
+    todo = client.createTodo(todo_text)
+    return todo.id
 
 
-def get_all_todos(stub):
-    return stub.readTodos(todo_pb2.noParam())
-
-
-def run():
-    with grpc.insecure_channel("localhost:40000") as channel:
-        stub = todo_pb2_grpc.TodoStub(channel)
-        print("Creating todo1: \n")
-        print(add_todo(stub, "Cooking"))
-        print("Creating todo2: \n")
-        print(add_todo(stub, "Cycling"))
-        print("All todos created: \n")
-        print(get_all_todos(stub))
-
-
-if __name__ == "__main__":
-    logging.basicConfig()
-    run()
+@app.route("/")
+def get_all_todos():
+    todos = client.readTodos(todo_pb2.noParam())
+    return render_template(
+        "homepage.html",
+        todo_items=todos.items
+    )
